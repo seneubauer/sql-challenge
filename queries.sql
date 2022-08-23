86de0b971654f339d5a1a089ec64fa6093c99fac
Scott Neubauer <scotteneubauer@gmail.com>
2022-08-22 22:15:43 -0500

    Final edits to queries file

--- queries.sql
+++ queries.sql
@@ -23,3 +23,32 @@ from employees as e
 inner join dept_emp as de on e.emp_no = de.emp_no
 inner join departments as d on de.dept_no = d.dept_no
 order by e.emp_no asc;
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select e.first_name, e.last_name, e.sex
+from employees as e
+where e.first_name = 'Hercules'
+and e.last_name like 'B%';
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e
+inner join dept_emp as de on e.emp_no = de.emp_no
+inner join departments as d on de.dept_no = d.dept_no
+where d.dept_name = 'Sales'
+order by e.emp_no asc;
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e
+inner join dept_emp as de on e.emp_no = de.emp_no
+inner join departments as d on de.dept_no = d.dept_no
+where d.dept_name = 'Sales'
+or d.dept_name = 'Development'
+order by e.emp_no asc;
+
+-- 8. List the frequency count of employee last names (i.e., how many employees share each last name) in descending order.
+select e.last_name, count(e.emp_no)
+from employees as e
+group by e.last_name
+order by count(e.last_name);
